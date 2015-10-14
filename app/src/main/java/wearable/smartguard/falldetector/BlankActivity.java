aebb621f619e7e08c379cabcb9a20b8b295cdfb8
jtalusan <jptalusan@gmail.com>
2015-10-14 20:43:51 +0800

    added counter for every 5 second gather must implement peaks next (peak detection or min/max)

--- app/src/main/java/wearable/smartguard/falldetector/BlankActivity.java
+++ app/src/main/java/wearable/smartguard/falldetector/BlankActivity.java
@@ -9,8 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class BlankActivity extends AppCompatActivity {
+    private static String DEBUG_TAG = "Activity";
+    private Button startStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,27 @@ public class BlankActivity extends AppCompatActivity {
         Log.d("TAG", "Start");
         AlarmManager scheduler = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(getApplicationContext(), AccelerometerSensorService.class );
+        intent.putExtra("Active", true);
         PendingIntent scheduledIntent = PendingIntent.getService(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
         scheduler.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 10000, scheduledIntent);
 
+        startStop = (Button) findViewById(R.id.startStop);
+        startStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AccelerometerSensorService.class );
+                intent.putExtra("Active", false);
+                AlarmManager scheduler = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                PendingIntent scheduledIntent = PendingIntent.getService(getApplicationContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                scheduler.cancel(scheduledIntent);
+//                scheduler.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 10000, scheduledIntent);
+//                PendingIntent scheduledIntent = PendingIntent.getService(getApplicationContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//                AlarmManager alarmManagerstop = (AlarmManager) getSystemService(ALARM_SERVICE);
+//                alarmManagerstop.cancel(scheduledIntent);
+//                scheduledIntent.cancel();
+                Log.d(DEBUG_TAG, "Alarm is stopped");
+            }
+        });
 //        Intent intent = new Intent(getApplicationContext(), AccelerometerSensorService.class );
 //        startService(intent);
     }
